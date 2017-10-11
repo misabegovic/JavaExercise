6b09f61b1bc994f965f8a67f7c3ed969fba20dc0
Muhamed Isabegovic <m.isabegovic@hotmail.com>
2017-10-11 23:20:51 +0200

    tinkering

--- ASourceFolder/Fetch.java
+++ ASourceFolder/Fetch.java
@@ -1,7 +1,12 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.MessageDigest;
@@ -12,10 +17,15 @@ import java.util.Base64;
 public class Fetch {
 	protected ArrayList<String> listOfBase64Encoded;
 	protected ArrayList<String> listOfChecksumString;
+	protected int numOfLinks = 0;
 	
 	public Fetch(){
 		this.listOfBase64Encoded = new ArrayList<String>();
 		this.listOfChecksumString = new ArrayList<String>();
+	}
+	
+	public int getNumOfLinks(){
+		return this.numOfLinks;
 	}
 	
 	public ArrayList<String> getBase64List(){
@@ -41,9 +51,31 @@ public class Fetch {
 	     }
 	     in.close();
 	    //System.out.println(getMD5Checksum(aBuilder.toString()));
-	    baseEncoder(aBuilder.toString());
-	    checksumEncoderFromBase(aBuilder.toString());
-	    
+	    //baseEncoder(aBuilder.toString());
+	    //checksumEncoderFromBase(aBuilder.toString());
+	    //File aFile = new File("Link"+(this.numOfLinks+1));
+	    BufferedWriter bw = null;
+		FileWriter fw = null; 
+		
+		try {
+			this.numOfLinks++;
+			fw = new FileWriter("Link"+this.numOfLinks);
+			
+			PrintWriter writer = new PrintWriter("Link"+this.numOfLinks);
+			writer.print("");
+			writer.close();
+			
+			bw = new BufferedWriter(fw);
+			bw.write(aBuilder.toString());
+			
+			bw.close();
+			fw.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
 	}
 
 
@@ -58,7 +90,7 @@ public class Fetch {
 	}
 
 
-   public static byte[] createChecksum(String filename) throws Exception {
+   public byte[] createChecksum(String filename) throws Exception {
      InputStream fis =  new FileInputStream(filename);
 
      byte[] buffer = new byte[1024];
@@ -76,13 +108,13 @@ public class Fetch {
      return complete.digest();
    }
 
-   //public String getMD5Checksum(String filename) throws Exception {
-     //  byte[] b = createChecksum(filename);
-       //String result = "";
+   public String getMD5Checksum(String filename) throws Exception {
+       byte[] b = createChecksum(filename);
+       String result = "";
 
-       //for (int i=0; i < b.length; i++) {
-         //  result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
-       //}
-       //return result;
-   //}
+       for (int i=0; i < b.length; i++) {
+          result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+       }
+       return result;
+   }
 }
--- ASourceFolder/Main.java
+++ ASourceFolder/Main.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Base64;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		File aFile = new File("TextWithLinks");
 
 	   	Validator aCheck = new Validator();
@@ -33,8 +34,15 @@ public class Main {
 	      }catch(Exception e){}
 	    }
 	    
-	    System.out.println(fData.getBase64List());
-	    System.out.println(fData.getChecksumList());
+	    for(int i = 1; i <= fData.getNumOfLinks(); i++){
+	    	String md5 = fData.getMD5Checksum("Link"+i);
+	    	byte[] encodedBuilder = Base64.getEncoder().encode(md5.getBytes());
+	    	System.out.println(md5);
+	    	System.out.println(encodedBuilder);
+	    }
+	    
+	    //System.out.println(fData.getBase64List());
+	    //System.out.println(fData.getChecksumList());
 	}
 	
 }
